5268e539818d9343b0f4ced38609fa3ada74c220
David-JaeHyuk <aspact@naver.com>
2017-02-03 18:02:37 +0900

    문제 29 - 구두 수선공 문제(Shoemaker's Problem) 해결

---
+++ Algorithm_Practice/src/Test/ListAddRemoveTest.java
@@ -0,0 +1,18 @@
+package Test;
+
+import java.util.ArrayList;
+
+public class ListAddRemoveTest 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		list.add(1);
+		
+	}
+
+}
